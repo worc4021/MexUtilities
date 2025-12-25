6f0f5b28ecd3374dbbe3549dff3d60003fdb3b00
Manuel Schaich <rmschaich@outlook.com>
2025-12-25 10:12:33 +0000

    Add basetest to find the binaries properly

--- test/matlab/cases.m
+++ test/matlab/cases.m
@@ -1,33 +1,4 @@
-classdef cases < matlab.unittest.TestCase
-   
-    methods(TestClassSetup)
-        
-        function setPathUp(testCase)
-            repoPath = fileparts(fileparts(fileparts(mfilename('fullpath'))));
-            availablePaths = dir(fullfile(repoPath,'out','build','*','CMakeCache.txt'));
-            if isscalar(availablePaths)
-                bindir = fullfile(availablePaths.folder,'test');
-            else
-                dates = datetime(arrayfun(@(x)x.date,availablePaths,'UniformOutput',false));
-                [~,i] = sort(dates,1,"descend");
-                bindir = fullfile(availablePaths(i(1)).folder,'test');
-            end
-            
-            % Set up shared state for all tests. 
-            addpath(bindir);
-            testCase.addTeardown(@()rmpath(bindir))
-            
-            % Tear down with testCase.addTeardown.
-        end
-        % Shared setup for the entire test class
-        
-
-
-    end
-    
-    methods(TestMethodSetup)
-        % Setup for each test
-    end
+classdef cases < baseTest
     
     methods(Test)
         function isfieldTest(testCase)
